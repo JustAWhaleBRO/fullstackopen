b8f9ae993f870e1160a764f1810100d17ad7f4ca
justawhale <tv.kiet03@gmail.com>
2026-04-08 08:29:58 +0200

    uploaded static files from frontend to backend

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -36,8 +36,8 @@ const App = () => {
 
   const toggleImportanceOf = (id) => {
     const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important}
-    
+    const changedNote = { ...note, important: !note.important }
+
     noteServices.update(id, changedNote)
       .then(returnedNote => {
         setNotes(
@@ -49,9 +49,10 @@ const App = () => {
           `Note '${note.content}' was already removed from server`
         )
         setTimeout(() => {
-          setErrorMessage(null)}, 5000)
-        })
-        setNotes(notes.filter(n => n.id !== id))
+          setErrorMessage(null)
+        }, 5000)
+      })
+    setNotes(notes.filter(n => n.id !== id))
   }
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important)
@@ -67,8 +68,8 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note 
-            key={note.id} 
+          <Note
+            key={note.id}
             note={note}
             toggleImportance={() => toggleImportanceOf(note.id)}
           />
