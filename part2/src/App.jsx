251c1df5228ad8f25328eb014714144a60805ea7
justawhale <tv.kiet03@gmail.com>
2026-05-27 20:52:10 +0200

    started part5

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,17 +1,21 @@
 import { useState, useEffect } from 'react'
 import Footer from './components/Footer'
 import Note from './components/Note'
-import noteServices from './services/notes'
+import noteService from './services/notes'
+import loginService from './services/login'
 import Notification from './components/Notification'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-    noteServices.getAll().then(initialNotes => {
+    noteService.getAll().then(initialNotes => {
       setNotes(initialNotes)
     })
   }, [])
@@ -23,7 +27,7 @@ const App = () => {
       important: Math.random() > 0.5,
     }
 
-    noteServices.create(noteObject)
+    noteService.create(noteObject)
       .then(returnedNote => {
         setNotes(notes.concat(returnedNote))
         setNewNote('')
@@ -38,7 +42,7 @@ const App = () => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
-    noteServices.update(id, changedNote)
+    noteService.update(id, changedNote)
       .then(returnedNote => {
         setNotes(
           notes.map(note => note.id === id ? returnedNote : note)
@@ -46,7 +50,7 @@ const App = () => {
       })
       .catch(error => {
         setErrorMessage(
-          `Note '${note.content}' was already removed from server`
+          `Note '${note.content}' was already removed from server: ${error}`
         )
         setTimeout(() => {
           setErrorMessage(null)
@@ -55,12 +59,74 @@ const App = () => {
       })
   }
 
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+      noteService.setToken(user)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
   const notesToShow = showAll ? notes : notes.filter((note) => note.important)
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>
+          username
+          <input
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange} />
+      <button type="submit">save</button>
+    </form>
+  )
 
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+
+      {!user && loginForm()}
+      {user && (
+        <div>
+          <p>{user.name} logged in</p>
+          {noteForm()}
+        </div>
+      )}
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -75,10 +141,6 @@ const App = () => {
           />
         ))}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
       <Footer />
     </div>
   )
---
+++ part2/src/services/login.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+const baseUrl = '/api/login'
+
+const login = async credentials => {
+    const resp = await axios.post(baseUrl, credentials)
+    return resp.data
+}
+
+export default { login }
+
+
